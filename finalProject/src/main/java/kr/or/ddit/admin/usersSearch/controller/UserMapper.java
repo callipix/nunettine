e8993f622b01030c20d9226c6f6dbf6a0e896173
jaehoon <icelift@naver.com>
2024-08-12 11:21:59 +0900

    modify project code refactoring2

--- finalProject/src/main/java/kr/or/ddit/admin/usersSearch/controller/UserMapper.java
+++
@@ -1,6 +0,0 @@
-package kr.or.ddit.admin.usersSearch.controller;
-
-public interface UserMapper {
-
-	public String userType(String userId);
-}
--- finalProject/src/main/java/kr/or/ddit/board/pro_story/service/ImgUpload.java
+++
@@ -1,8 +0,0 @@
-package kr.or.ddit.board.pro_story.service;
-
-public interface ImgUpload {
-
-	int upload();
-	
-	
-}
